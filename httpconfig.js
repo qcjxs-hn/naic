d5d72ec168016da35005d573ed53e762b5388ed7
青春进行时 <8411351+qcjxsbf@user.noreply.gitee.com>
2024-02-14 19:54:52 +0800

    t

---
+++ httpconfig.js
@@ -0,0 +1,15 @@
+const apiConfig={
+	dev:{
+		baseUrl:'http://127.0.0.1:8084' //http://127.0.0.1:8084
+	},
+	prod:{
+		baseUrl:'http://hhh.free.idcfengye.com'
+	},
+	process:{
+		ENCRYPTION_KEY:"1871982008200171"
+	},
+	tp:{
+		Url:"http://naic.asia/",
+	}
+}
+export default apiConfig
